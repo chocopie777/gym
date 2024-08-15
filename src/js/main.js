ff86fd0a3e206b4cd1eac8850bd7acf74f4cfbce
chocopie777 <egoran99@gmail.com>
2024-08-15 22:31:18 +0500

    verstka reviews, questions, map, footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,13 @@
 import './libs.js';
 import $ from "jquery";
+import '../../node_modules/inputmask/dist/jquery.inputmask.js';
 
-$('.reviews-slider').slick();
+$('.reviews-slider').slick({
+    arrows: false,
+    fade: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+});
+$('#phone').inputmask({"mask": "+7 (999) 999-99-99"});
+$('#email').inputmask('email');
