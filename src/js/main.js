02870e7c4a802e4ed081bd166c26b2a0526dbe5b
chocopie777 <egoran99@gmail.com>
2024-08-18 16:05:01 +0500

    header nav adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -9,5 +9,12 @@ $('.reviews-slider').slick({
     autoplay: true,
     autoplaySpeed: 5000,
 });
+
 $('#phone').inputmask({"mask": "+7 (999) 999-99-99"});
 $('#email').inputmask('email');
+
+const burger = document.querySelector('.burger-btn');
+const menu = document.querySelector('.menu__list');
+burger.addEventListener('click', () => {
+    menu.classList.toggle('show-menu');
+})
