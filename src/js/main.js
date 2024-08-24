9d01f0c73da4512aa9906d45441621b0f6e3c476
chocopie777 <egoran99@gmail.com>
2024-08-24 18:30:19 +0500

    smooth scroll anchor

--- src/js/main.js
+++ src/js/main.js
@@ -17,4 +17,19 @@ const burger = document.querySelector('.burger-btn');
 const menu = document.querySelector('.header .menu__list');
 burger.addEventListener('click', () => {
     menu.classList.toggle('show-menu');
-})
+});
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+for(let anchor of anchors) {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const blockID = anchor.getAttribute('href').substr(1);
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+            
+        })
+    });
+}
